d4aabea9157f25983b3877dd9601ced963c6539a
luoluo960127-commits <luoluo960127@gmail.com>
2026-04-28 11:03:11 +0800

    1.1

--- sketch.js
+++ sketch.js
@@ -1,4 +1,11 @@
 let capture;
+let handPose;
+let hands = [];
+
+function preload() {
+  // 載入 handPose 模型
+  handPose = ml5.handPose();
+}
 
 function setup() {
   // 產生一個全螢幕的畫布
@@ -7,6 +14,14 @@ function setup() {
   capture = createCapture(VIDEO);
   // 隱藏原始的 HTML video 標籤，只在畫布上繪製
   capture.hide();
+
+  // 開始偵測影像中的手部
+  handPose.detectStart(capture, gotHands);
+}
+
+function gotHands(results) {
+  // 將偵測結果存入 hands 變數
+  hands = results;
 }
 
 function draw() {
@@ -23,6 +38,22 @@ function draw() {
 
   // 將影像繪製在畫布中間
   image(capture, x, y, vW, vH);
+
+  // 繪製手部辨識點
+  for (let i = 0; i < hands.length; i++) {
+    let hand = hands[i];
+    for (let j = 0; j < hand.keypoints.length; j++) {
+      let keypoint = hand.keypoints[j];
+      
+      // 因為影像被縮放成 50% 且位移到中間，所以座標需要重新映射
+      let drawX = map(keypoint.x, 0, capture.width, x, x + vW);
+      let drawY = map(keypoint.y, 0, capture.height, y, y + vH);
+
+      fill(0, 255, 0);
+      noStroke();
+      circle(drawX, drawY, 8);
+    }
+  }
 }
 
 function windowResized() {
